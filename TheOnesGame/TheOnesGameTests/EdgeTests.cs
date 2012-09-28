50d3758511c416a8bb8a5c6b926c8d2dfd4a6509
Anna and Ammar <bla@bla.com>
2012-09-28 14:52:37 +0100

    Added acceptance test

--- TheOnesGame/TheOnesGameTests/EdgeTests.cs
+++ TheOnesGame/TheOnesGameTests/EdgeTests.cs
@@ -7,19 +7,19 @@ namespace TheOnesGameTests
     [TestFixture]
     public class EdgeTests
     {
-//       [Test]
-//        public void A_two_point_user_story_on_position_1_is_moved_to_done()
-//        {
-//            var userStory = new UserStory(2);
-//            var edge = new Edge();
-//
-//            edge.SetPosition(1, userStory);
-//                Assert.That(edge.CompleteStory, Is.EqualTo(null));
+       [Test]
+        public void A_two_point_user_story_on_position_1_is_moved_to_done()
+        {
+            var userStory = new UserStory(2);
+            var edge = new Edge();
 
-//            edge.MoveStoryOn(1);
-//
-//            Assert.That(edge.CompleteStory, Is.EqualTo(userStory));
-//        }
+            edge.SetPosition(1, userStory);
+                Assert.That(edge.CompleteStory, Is.EqualTo(null));
+
+            edge.MoveStoryOn(1);
+
+            Assert.That(edge.CompleteStory, Is.EqualTo(userStory));
+        }
 
         [Test]
         public void User_story_position_can_be_set_on_an_edge()
