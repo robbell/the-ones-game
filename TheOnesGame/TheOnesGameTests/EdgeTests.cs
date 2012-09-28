077a3c7dec9b79735c1729c6cfedbab0868fb55a
Anna and Ammar <bla@bla.com>
2012-09-28 13:52:18 +0100

    Accepting server changes

--- TheOnesGame/TheOnesGameTests/EdgeTests.cs
+++ TheOnesGame/TheOnesGameTests/EdgeTests.cs
@@ -6,7 +6,7 @@ namespace TheOnesGameTests
     [TestFixture]
     public class EdgeTests
     {
-//        [Test]
+//       [Test]
 //        public void A_two_point_user_story_on_position_1_is_moved_to_done()
 //        {
 //            var userStory = new UserStory(2);
@@ -30,6 +30,7 @@ namespace TheOnesGameTests
             Assert.That(edge.GetStoryOnPosition(1), Is.EqualTo(userStory));
         }
 
+
         [Test]
         public void User_story_can_be_moved_from_given_position_to_the_next_position()
         {
@@ -41,7 +42,7 @@ namespace TheOnesGameTests
             edge.MoveStoryOn(startingPosition);
             Assert.That(edge.GetStoryOnPosition(startingPosition+1), Is.EqualTo(userStory));
         }
-//        
+
 //        [Test]
 //        public void User_story_position_can_be_set_on_an_edge()
 //        {
