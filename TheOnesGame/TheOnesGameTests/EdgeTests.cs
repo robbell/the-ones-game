a73ecc40d615cebea1985f1595445df7e774f6c5
Anna and Ammar <bla@bla.com>
2012-09-28 10:32:20 +0100

    Fix and new Ignore list

--- TheOnesGame/TheOnesGameTests/EdgeTests.cs
+++ TheOnesGame/TheOnesGameTests/EdgeTests.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace TheOnesGameTests
 {
-
+    [TestFixture]
     public class EdgeTests
     {
     }
